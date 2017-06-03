7b6a55a7d452d8e67751938393a578f7e6aefb84
Niklas K <drdaxxy@googlemail.com>
2017-06-03 00:45:59 +0000

    basic script listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def friendly_stage_name(db_name)
+    table = {
+      "untouched" => "Untouched",
+      "translation" => "In translation",
+      "tlc" => "TLC",
+      "editing" => "Editing",
+      "review" => "TL review",
+      "finalised" => "Finalised"
+    }
+    return table[db_name]
+  end
 end
